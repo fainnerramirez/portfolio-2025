b4121d364bbd1ca735d9d86c57ff4b2b9a538b59
Faidev <fainnerramirez@gmail.com>
2025-01-28 20:44:10 -0500

    feat: add notfound page component

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,23 +1,25 @@
 import { Button, Heading, useColorModeValue, VStack } from "@chakra-ui/react";
 
 export const NotFoundPage: React.FC = () => {
+  const goToHome = () => {
+    window.location.href = "/";
+  };
 
-    const gotToHome = () => {
-        window.location.href = "/";
-    }
-
-    return (
-        <VStack display={"flex"} justifyContent={"center"} alignItems={"center"} spacing={5}>
-            <Heading>
-                Página no encontrada
-            </Heading>    
-            <Button 
-                variant={"solid"} 
-                colorScheme={useColorModeValue("teal", "cyan")}
-                onClick={gotToHome}
-            >
-                Ir a página principal
-            </Button>
-        </VStack>
-    )
-}
+  return (
+    <VStack
+      display={"flex"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      spacing={5}
+    >
+      <Heading>Página no encontrada</Heading>
+      <Button
+        variant={"solid"}
+        colorScheme={useColorModeValue("teal", "cyan")}
+        onClick={goToHome}
+      >
+        Ir a página principal
+      </Button>
+    </VStack>
+  );
+};
