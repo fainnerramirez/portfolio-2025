362bd7cf4b0394c4845d43f491faaff8bf2eeea5
Fainner Ramírez <fainner.ramirez@dentsu.com>
2025-03-02 22:46:59 -0500

    feat: delete route project

--- src/routes/route-app.tsx
+++ src/routes/route-app.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { NotFoundPage } from "../components/NotFound";
 import HomePage from "../pages/home-page";
-import ProjectsPage from "../pages/projects-page";
 import { GithubPage } from "../pages/github-page";
 
 export const RoutesApp = () => {
@@ -9,7 +8,6 @@ export const RoutesApp = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/proyects" element={<ProjectsPage />} />
         <Route path="/github" element={<GithubPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
